5fc9c9575ea0cf5bfad390dfc73ce72e03b5068d
Sonw00 <thsdndud000@gmail.com>
2026-05-01 21:18:54 +0900

    resolve CI error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   base: '/blog/',
 })
